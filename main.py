fff3f2cbb432f98afd307e5dc6e5e817f51f9092
Chingiz <mister.abiev@mail.ru>
2022-10-06 19:59:10 +0600

    file d

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("Bugagaga")
-x=5
-print(x)
+import numpy as np
+import cv2 as cv
+img = np.zeros((512,512,3), np.uint8)
+img2 = np.zeros((512,512,3), np.uint8)
+cv.line(img,(200,200),(300,50),(75,45,74),8)
+cv.line(img,(400,200),(300,50),(75,45,74),8)
+cv.line(img,(0,400),(512,400),(0,255,0),8)
+cv.rectangle(img,(200,200),(400,400),(14,145,255),8)
+cv.rectangle(img,(300,300),(250,250),(255,255,255),5)
+font = cv.FONT_HERSHEY_SIMPLEX
+cv.imshow("original", img)
+cv.waitKey()
